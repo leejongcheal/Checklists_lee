fc7cf95efb38608ad72ba1d14264aaabc6205f6a
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-03-30 16:51:32 +0900

    Make one line of table cell

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -14,6 +14,21 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "ChecklistItem",for: indexPath)
+            
+            return cell
+    }
 
 
 }
