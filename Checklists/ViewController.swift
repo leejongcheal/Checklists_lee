a0171b35c96ebba69d0740a5d8d618f66c57c9fc
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-03-30 12:37:31 +0900

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Myoung-Wan Koo on 30/03/2019.
+//  Copyright © 2019 Myoung-Wan Koo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
