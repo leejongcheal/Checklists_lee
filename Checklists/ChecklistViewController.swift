59c16850112b3dc955c32fc12f93d9076c1c2a96
leejongcheal <aksndk123@naver.com>
2019-04-11 00:21:21 +0900

    테이블뷰2 완성    기본뷰함수에 리턴값바꺼서 출력 라벨 수 바꾸고 indexPath를 가지고 몇번쨰에 무슨 값을 줄것인지 설정함

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -18,7 +18,7 @@ class ChecklistViewController: UITableViewController {
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return 1 
+        return 100
     }
     
     override func tableView(_ tableView: UITableView,
@@ -27,6 +27,20 @@ class ChecklistViewController: UITableViewController {
             let cell = tableView.dequeueReusableCell(
                 withIdentifier: "ChecklistItem",for: indexPath)
             
+            let label = cell.viewWithTag(1000) as! UILabel
+            
+            if indexPath.row % 5 == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row % 5 == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row % 5 == 2 {
+                label.text = "Learn iOS development"
+            } else if indexPath.row % 5 == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row % 5 == 4 {
+                label.text = "Eat ice cream"
+            }
+
             return cell
     }
 
