20eb8a4593bba07c974ee3c4821c8f5bf98a4c6e
leejongcheal <aksndk123@naver.com>
2019-04-11 03:30:15 +0900

    6 완료. 스태틱 셀 로 해주고 3칸중 2칸셀 지우고 스타일 그룹으로 바꺼줌(위랑 쫌 띠어져 나옴) 텍스트를 거기에 넣어줌  , 앱실행중에 입력할려하는데 키보드 안나오면 하드웨어가서 설정, 애드아이템 스위프트에 입력을 위한 함수 정의와 자동으로 키보드 칸가게하는 함수도 정의, 그리고 텍스 트 칸 설정해줌 placeholder 	는 빈칸에 기본적으로 보이는 글씨 설정해줌

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) { //  뷰에 바로 키보드 칸 올라가게 하는 함수
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     // MARK:- Actions
@@ -22,8 +29,17 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
     
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath)
+        -> IndexPath? {
+            return nil
+    }
     
 }
