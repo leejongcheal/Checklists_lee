e8edd169ab361966ea9878f75191c8408ecb188e
leejongcheal <aksndk123@naver.com>
2019-04-11 01:08:27 +0900

    3 toggle 추가및 item 변수 정의 및 사용 눌렀을떄 뷰함수 추가

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,20 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by 캡디 on 11/04/2019.
+//  Copyright © 2019 leejongcheal. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    /* For toggling */
+    func toggleChecked() {
+        checked = !checked
+    }
+}
+
